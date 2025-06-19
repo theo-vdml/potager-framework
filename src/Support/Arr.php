90a53cc7d90ce7774477d026e7077caeb214c923
Théo Vandormael <theo.vandormael@proton.me>
2025-06-19 13:41:32 +0200

    add wrap utility on array

--- src/Support/Arr.php
+++ src/Support/Arr.php
@@ -2,8 +2,22 @@
 
 namespace Potager\Support;
 
+/**
+ * Class Arr
+ *
+ * A utility class for manipulating arrays using dot notation.
+ *
+ * @package Potager\Support
+ */
 class Arr
 {
+    /**
+     * Determine if an array has the given key using dot notation.
+     *
+     * @param array $array The array to search.
+     * @param string $key The dot-notated key to check.
+     * @return bool True if the key exists, false otherwise.
+     */
     public static function has(array $array, string $key)
     {
         if (!$key)
@@ -17,6 +31,14 @@ class Arr
         return true;
     }
 
+    /**
+     * Retrieve a value from an array using dot notation.
+     *
+     * @param array $array The array to search.
+     * @param string $key The dot-notated key to retrieve.
+     * @param mixed $default The default value to return if the key doesn't exist.
+     * @return mixed The value from the array or the default value.
+     */
     public static function get(array $array, string $key, mixed $default = null)
     {
         if (!$key)
@@ -30,7 +52,14 @@ class Arr
         return $array;
     }
 
-    // Set a value in a array unsing dot notation
+    /**
+     * Set a value in an array using dot notation.
+     *
+     * @param array $array The array to modify (passed by reference).
+     * @param string $key The dot-notated key to set.
+     * @param mixed $value The value to set.
+     * @return void
+     */
     public static function set(array &$array, string $key, mixed $value)
     {
         $keys = explode('.', $key);
@@ -44,7 +73,13 @@ class Arr
         $array[array_shift($keys)] = $value;
     }
 
-    // Forget a key from an array using dot notation
+    /**
+     * Remove a value from an array using dot notation.
+     *
+     * @param array $array The array to modify (passed by reference).
+     * @param string $key The dot-notated key to remove.
+     * @return void
+     */
     public static function forget(array &$array, string $key)
     {
         $keys = explode('.', $key);
@@ -56,5 +91,19 @@ class Arr
             $array = &$array[$segment];
         }
         unset($array[array_shift($keys)]);
+    }
+
+    /**
+     * Wrap a value in an array if it is not already an array.
+     *
+     * @param mixed $value The value to wrap.
+     * @return array The value wrapped in an array.
+     */
+    public static function wrap(mixed $value): array
+    {
+        if (!is_array($value)) {
+            $value = [$value];
+        }
+        return $value;
     }
 }
